96506d013bce942424305f0b4ae9419fcc942ef5
Krzysztof Łęczycki <leczycki.krzysztoff@gmail.com>
2024-05-19 18:02:20 +0200

    no elo

--- Album.cpp
+++ Album.cpp
@@ -1,10 +1,17 @@
 #include "Album.h"
 
-Album::Album(int num) {
+Album::Album(string title, string artist, int num, vector<string> songs) {
 	this->numberOfTracks = num;
+	this->albumTitle = title;
+	this->albumArtist = artist;
+	this->songList = songs;
 }
 int Album::getNumberOfTracks() {
 	return this->numberOfTracks;
+}
+
+Album::Album(int num) {
+	this->numberOfTracks = num;
 }
 
 
